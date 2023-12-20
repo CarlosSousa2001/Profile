9a023687b6ec33bc73f7bcb6c2c1ea99d3e74779
carlos <carlosramos10k@gmail.com>
2023-12-20 12:00:30 -0300

    colocando container na root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Footer from "@/components/Footer/Footer"
 import Profile from "./Profile/page"
 import Header from "@/components/Header/Header"
+import style from "./Profile/style.module.css"
 
 const Page = () => {
     return (
-        <>
+        <div className={style.container}>
         <Header/>
         <Profile/>
         <Footer/>
-        </>
+        </div>
 
     )
 }
